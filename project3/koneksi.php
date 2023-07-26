014f7bcedd4f4ea7e8c024d80198d9e293b2d421
Mohammadananta <anantamohammad2003@gmail.com>
2023-07-26 21:24:50 +0700

    up

--- project3/koneksi.php
+++ project3/koneksi.php
@@ -3,11 +3,8 @@ $servername = "localhost";
 $database = "vsga";
 $username = "root";
 $password = "";
-
-// Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
-
-// Check connection
 if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
+?>
--- project4/koneksi.php
+++ project4/koneksi.php
@@ -3,9 +3,7 @@ $servername = "localhost";
 $database = "vsga";
 $username = "root";
 $password = "";
-// Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
-// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
